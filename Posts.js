7483d721e8feff3aaf6ed914cc991aef4d2aba60
Kevin J. Russell <KevinJRussell@gmail.com>
2017-01-15 20:15:12 -0600

    Added 'Filter Me' button

--- Posts.js
+++ Posts.js
@@ -1,7 +1,8 @@
 AddPostNumber();
+AddFilterMeButton();
 
 function AddPostNumber() {
-    var pageNumber = GetUrlParameter("page");
+    var pageNumber = GetUrlParameter('page');
     if (pageNumber == null)
         pageNumber = 1;
     var postFloor = (pageNumber - 1) * 50;
@@ -13,4 +14,18 @@ function AddPostNumber() {
 
         posts[i].getElementsByClassName('message-top')[0].appendChild(postNumber);
     }
+}
+
+function AddFilterMeButton() {
+    var userId = GetUserId();
+    var url = document.location + '&u=' + userId;
+    var infobar = document.getElementsByClassName('infobar')[0];
+    var filterMeButton = document.createElement('a');
+    filterMeButton.href = url;
+    filterMeButton.id = 'filterMe';
+    filterMeButton.innerHTML = 'Filter Me';
+    filterMeButton.style = 'text-decoration: none;';
+
+    infobar.insertBefore(document.createTextNode(' | '), infobar.firstChild);
+    infobar.insertBefore(filterMeButton, infobar.firstChild);
 }
--- Util.js
+++ Util.js
@@ -1,5 +1,16 @@
+function GetUserId() {
+    var userbar = document.getElementsByClassName('userbar')[0];
+    var url = userbar.getElementsByTagName('a')[0].getAttribute('href');
+
+    return GetParameter(url.split('?')[1], 'user');
+}
+
 function GetUrlParameter(key) {
-    var decodeUrl = decodeURIComponent(window.location.search.substring(1));
+    return GetParameter(window.location.search.substring(1), key);
+}
+
+function GetParameter(url, key) {
+    var decodeUrl = decodeURIComponent(url);
     var queryStrings = decodeUrl.split('&');
     var value = null;
 
