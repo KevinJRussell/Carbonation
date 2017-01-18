3b1c72b6fc1878215fe83a01adb848890cb49455
KevinJRussell <KevinJRussell@GMail.com>
2017-01-18 15:36:38 -0600

    Framework for popup and options added. Needs layout work

---
+++ Options/Options.js
@@ -0,0 +1,22 @@
+function saveOptions(e) {
+    e.preventDefault();
+    browser.storage.local.set({
+        blacklist: document.querySelector('#blacklist').value
+    });
+}
+
+function restoreOptions() {
+    function setCurrentChoice(result) {
+        document.querySelector('#blacklist').value = result.blacklist || 'example';
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
+
+    var getting = browser.storage.local.get('blacklist');
+    getting.then(setCurrentChoice, onError);
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
--- Topics.js
+++ Topics.js
@@ -16,36 +16,7 @@ function CreateLink_LastPost() {
             link_LastPost.innerHTML = '&gt;';
             link_LastPost.style = 'text-decoration: none;';
 
-            // var spacer = document.createElement('span');
-            // spacer.style = 'margin-left: 10px;';
-
-            // td.querySelector('.fr').appendChild(spacer);
             td.querySelector('.fr').appendChild(link_LastPost);
         }
     });
-
-    // for (var i = 1; i < trs.length; i++) {
-    //     var tr = trs[i];
-    //
-    //     var td = tr.querySelector('td');
-    //
-    //     if (td) {
-    //         var postCount = parseInt(td.parentNode.getElementsByTagName('td')[2].innerHTML.split(' ')[0]);
-    //         var lastPage = Math.ceil(postCount / POSTS_PER_PAGE);
-    //         var topic = td.parentNode.querySelector('td').querySelector('a');
-    //         var url_LastPost = topic.href + '&page=' + lastPage;
-    //
-    //         var link_LastPost = document.createElement('a');
-    //         link_LastPost.href = url_LastPost;
-    //         link_LastPost.id = 'lastPost';
-    //         link_LastPost.innerHTML = '&gt;';
-    //         link_LastPost.style = 'text-decoration: none;';
-    //
-    //         // var spacer = document.createElement('span');
-    //         // spacer.style = 'margin-left: 10px;';
-    //
-    //         // td.querySelector('.fr').appendChild(spacer);
-    //         td.querySelector('.fr').appendChild(link_LastPost);
-    //     }
-    // }
 }
