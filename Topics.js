aae2a7d16a77df5d1aa2711e4d9afa8ca5e8224e
Kevin J. Russell <KevinJRussell@gmail.com>
2018-02-23 23:14:39 -0600

    Refactored Topics.js. Functions now accept a single topic as a parameter instead of running their own loop, just like Posts.js. Also added blacklist functionality to Topic List Not very happy with the way this looks, or my hardcoded [1], but this should be functional.

--- Topics.js
+++ Topics.js
@@ -1,22 +1,55 @@
-CreateLink_LastPost();
+let settings;
+let blacklist;
 
-function CreateLink_LastPost() {
-    document.querySelectorAll('tr').forEach(function (tr) {
-        const td = tr.querySelector('td');
+GetSettings();
 
-        if (td) {
-            const postCount = parseInt(td.parentNode.getElementsByTagName('td')[2].innerHTML.split(' ')[0]);
-            const lastPage = Math.ceil(postCount / POSTS_PER_PAGE);
-            const topic = td.parentNode.querySelector('td').querySelector('a');
-            const url_LastPost = topic.href + '&page=' + lastPage;
+function GetSettings() {
+    const getSettings = browser.storage.local.get('blacklist');
 
-            const link_LastPost = document.createElement('a');
-            link_LastPost.href = url_LastPost;
-            link_LastPost.id = 'lastPost';
-            link_LastPost.innerHTML = '&gt;';
-            link_LastPost.style = 'text-decoration: none;';
+    getSettings.then(function (result) {
+        settings = result;
+        blacklist = settings.blacklist.split(',').map((user) => user.trim());
 
-            td.querySelector('.fr').appendChild(link_LastPost);
-        }
+        ProcessTopics();
     });
+}
+
+function ProcessTopics() {
+    const topics = document.querySelectorAll('tr');
+
+    topics.forEach((topic) => {
+        if (settings.blacklist)
+            BlockBlacklistedUsers(topic);
+
+        CreateLink_LastPost(topic);
+    });
+}
+
+function BlockBlacklistedUsers(topic) {
+    const temp = topic.querySelectorAll('td')[1];
+    if (temp == null) return;
+    username = temp.querySelector('a').innerHTML;
+
+    if (blacklist.includes(username)) {
+        topic.style = 'display: none;';
+    }
+}
+
+function CreateLink_LastPost(topic) {
+    const td = topic.querySelector('td');
+
+    if (td) {
+        const postCount = parseInt(td.parentNode.getElementsByTagName('td')[2].innerHTML.split(' ')[0]);
+        const lastPage = Math.ceil(postCount / POSTS_PER_PAGE);
+        const topic = td.parentNode.querySelector('td').querySelector('a');
+        const url_LastPost = topic.href + '&page=' + lastPage;
+
+        const link_LastPost = document.createElement('a');
+        link_LastPost.href = url_LastPost;
+        link_LastPost.id = 'lastPost';
+        link_LastPost.innerHTML = '&gt;';
+        link_LastPost.style = 'text-decoration: none;';
+
+        td.querySelector('.fr').appendChild(link_LastPost);
+    }
 }
