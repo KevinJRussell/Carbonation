c0bdde9ad4c6373d8a1feb816e5e7d7d31183d6b
Kevin J. Russell <KevinJRussell@gmail.com>
2018-02-23 21:52:52 -0600

    Added links to Message History, Post New Message Page, and IRC channel when an error shows up. Should really work on formatting one day

--- Globals.js
+++ Globals.js
@@ -2,6 +2,7 @@
 const POSTS_PER_PAGE = 50;
 
 AddMessageHistoryButton();
+ErrorPage();
 
 // Global page mods:
 function AddMessageHistoryButton() {
@@ -23,4 +24,38 @@ function AddMessageHistoryButton() {
         bookmarks.insertBefore(document.createTextNode(' | '), bookmarks.firstChild);
         bookmarks.insertBefore(messageHistoryButton, bookmarks.firstChild);
     });
+}
+
+function ErrorPage() {
+    if (document.body.style.backgroundImage.indexOf('errorlinks.png') == -1)
+        return;
+
+    var carbonationError = document.createElement('div');
+    carbonationError.style.top = '40%';
+    carbonationError.style.left = '40%';
+    carbonationError.style.position = 'absolute';
+
+    var errorMessage = document.createElement('a');
+    errorMessage.href = '/history.php';
+    errorMessage.id = 'historyLink';
+    errorMessage.innerHTML = 'Browse Message History';
+
+    var newMessage = document.createElement('a');
+    newMessage.href = '/postmsg.php?tag=LUE';
+    newMessage.id = 'newMessageLink';
+    newMessage.innerHTML = 'Post New Message';
+
+    var irc = document.createElement('a');
+    irc.href = 'https://qchat.rizon.net/?channels=ETI';
+    irc.target = '_blank';
+    irc.id = 'ircLink';
+    irc.innerHTML = 'Come chat on IRC while ETI is down!';
+
+    carbonationError.insertBefore(irc, carbonationError.firstChild);
+    carbonationError.insertBefore(document.createElement('br'), carbonationError.firstChild);
+    carbonationError.insertBefore(newMessage, carbonationError.firstChild);
+    carbonationError.insertBefore(document.createElement('br'), carbonationError.firstChild);
+    carbonationError.insertBefore(errorMessage, carbonationError.firstChild);
+
+    document.body.appendChild(carbonationError);
 }
